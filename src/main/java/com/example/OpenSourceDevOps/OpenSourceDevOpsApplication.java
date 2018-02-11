5e5231ea61913687cd9fae5a7a202d340326331f
c133373 <Casey.O'Kane@8451.com>
2018-02-10 20:38:28 -0500

    Initial Commit with Spring Initialzr Project

---
+++ src/main/java/com/example/OpenSourceDevOps/OpenSourceDevOpsApplication.java
@@ -0,0 +1,10 @@
+package com.example.OpenSourceDevOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSourceDevOpsApplication {
+
+	public static void main(String[] args) { SpringApplication.run(OpenSourceDevOpsApplication.class, args); }
+}
---
+++ src/test/java/com/example/OpenSourceDevOps/OpenSourceDevOpsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.OpenSourceDevOps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpenSourceDevOpsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
